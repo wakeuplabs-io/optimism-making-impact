a4aefad44e8659f4f7457790e07405c99c778eb7
MartinC <59508264+martinc1991@users.noreply.github.com>
2024-12-19 10:44:00 -0300

    Format whole project

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,27 +1,27 @@
-import cors from "cors";
-import express from "express";
-import helmet from "helmet";
-import morgan from "morgan";
+import cors from 'cors';
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
-import serverless from "serverless-http";
+import serverless from 'serverless-http';
 
-import envParsed from "@/envParsed.js";
-import middlewares from "@/middlewares/index.js";
-import routes from "@/routes/index.js";
+import envParsed from '@/envParsed.js';
+import middlewares from '@/middlewares/index.js';
+import routes from '@/routes/index.js';
 
 const app = express();
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", routes);
+app.use('/api', routes);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
-if (envParsed().NODE_ENV === "development") {
+if (envParsed().NODE_ENV === 'development') {
   app.listen(envParsed().PORT, () => {
     console.log(`App Started at PORT=${envParsed().PORT}`);
   });
