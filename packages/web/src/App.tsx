9f08d68fc232867ae8fd531bbe40acab73bbe8f3
Nicolas <nicopuegher@gmail.com>
2024-12-19 15:19:39 -0300

    Change App from default to named export and update its references

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,4 +1,4 @@
-function App() {
+export function App() {
   return (
     <div className="flex h-screen flex-col">
       <nav className="absolute bg-green-100">Sidebar</nav>
@@ -7,5 +7,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
