212ddac797f31e714d510e6039f1ed8a0d9c9581
Nicolas <nicopuegher@gmail.com>
2024-12-19 15:46:18 -0300

    Add layout styles for desktop

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,9 +1,11 @@
 export function App() {
   return (
-    <div className="flex h-screen flex-col">
-      <nav className="absolute bg-green-100">Sidebar</nav>
-      <header className="bg-blue-100">Steps</header>
-      <main className="flex-1 bg-red-100">Layouts</main>
+    <div className="flex h-screen flex-col lg:flex-row">
+      <nav className="absolute bg-green-100 lg:static lg:h-full lg:w-[320px]">Sidebar</nav>
+      <div className="flex flex-1 flex-col">
+        <header className="bg-blue-100">Steps</header>
+        <main className="flex-1 bg-red-100">Layouts</main>
+      </div>
     </div>
   );
 }
