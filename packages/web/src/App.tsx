bdc07364c2c4ed7e1ff87d8e8342bae57d4ddb69
Nicolas <nicopuegher@gmail.com>
2024-12-19 15:18:49 -0300

    Create the skeleton for the primary layouts

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,12 +1,9 @@
-import { Button } from '@/components/ui/button';
-import { useState } from 'react';
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div>
-      <Button onClick={() => setCount((count) => count + 1)}>Count is {count}</Button>
+    <div className="flex h-screen flex-col">
+      <nav className="absolute bg-green-100">Sidebar</nav>
+      <header className="bg-blue-100">Steps</header>
+      <main className="flex-1 bg-red-100">Layouts</main>
     </div>
   );
 }
