45e550917f1097d15dd2b05517503ec206054470
Nicolas <nicopuegher@gmail.com>
2024-12-20 16:22:06 -0300

    Import and use the new logo component in sidebar

--- packages/web/src/layouts/sidebar/index.tsx
+++ packages/web/src/layouts/sidebar/index.tsx
@@ -1,7 +1,11 @@
+import { Logo } from '@/layouts/sidebar/components/logo';
+
 export function Sidebar() {
   return (
     <nav className="absolute h-full w-[320px] bg-white-high p-12 lg:static">
-      <div className="flex h-full flex-col gap-6">Sidebar</div>
+      <div className="flex h-full flex-col gap-6">
+        <Logo />
+      </div>
     </nav>
   );
 }
