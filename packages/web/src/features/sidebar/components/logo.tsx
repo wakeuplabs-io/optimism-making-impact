7ce458a7df94029c5c1388063ed39de6687d7f93
Nicolas <nicopuegher@gmail.com>
2024-12-20 17:39:59 -0300

    Use all text for the logotype

--- packages/web/src/features/sidebar/components/logo.tsx
+++ packages/web/src/features/sidebar/components/logo.tsx
@@ -1,14 +1,8 @@
-import letterO from '@/features/sidebar/assets/logo/letter-o.png';
-import letterP from '@/features/sidebar/assets/logo/letter-p.png';
-
 export function Logo() {
-  const logoStyles = 'h-[22px] w-[23px] 2xl:h-[24px] 2xl:w-[25px]';
-
   return (
-    <div className="flex h-6 gap-0.5">
-      <img className={logoStyles} src={letterO} alt="Letter O" />
-      <img className={logoStyles} src={letterP} alt="Letter P" />
-      <h1 className="2xl:mt-[1px]">Impact</h1>
-    </div>
+    <h1>
+      <span className="text-3xl text-primary 2xl:text-[34px]">O P</span>
+      <span className="ml-1.5">Impact</span>
+    </h1>
   );
 }
