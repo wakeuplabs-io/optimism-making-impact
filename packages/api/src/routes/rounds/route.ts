c09c87b50c1dfe47c1b0ce4a4192569423edcb51
Nicolas <nicopuegher@gmail.com>
2024-12-20 19:45:28 -0300

    Add a post request to create a new round from the backoffice

--- packages/api/src/routes/rounds/route.ts
+++ packages/api/src/routes/rounds/route.ts
@@ -17,3 +17,23 @@ roundsRouter.get('/', async (req: Request, res: Response, next: NextFunction) =>
     next(error);
   }
 });
+
+roundsRouter.post('/', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    // Database will change IDS from strings to integers, change this after that!
+    const totalRounds = await prisma.round.count();
+    const nextRoundNumber = totalRounds + 1;
+    // ========
+
+    await prisma.round.create({
+      data: {
+        name: `Round ${nextRoundNumber}`,
+        icon: 'DELETE_THIS',
+      },
+    });
+
+    apiResponse.success(res, { message: 'Round created successfully' }, 201);
+  } catch (error) {
+    next(error);
+  }
+});
