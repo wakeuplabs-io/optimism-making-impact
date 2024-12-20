72861b28de33c04f48733009cdf6c4b70d89e39d
MartinC <59508264+martinc1991@users.noreply.github.com>
2024-12-20 13:50:10 -0300

    Fetch steps from backend

--- packages/web/src/layout/sections/steps.tsx
+++ packages/web/src/layout/sections/steps.tsx
@@ -1,18 +1,55 @@
 import { Button } from '@/components/ui/button';
-import { useStepsStore } from '@/state';
+import { useSidebarStore, useStepsStore } from '@/state';
+import { useEffect } from 'react';
 
 export function StepsSection() {
-  const stepsState = useStepsStore((state) => state);
+  const setSteps = useStepsStore((state) => state.setSteps);
+  const selectedRound = useSidebarStore((state) => state.selectedRound);
+
+  useEffect(() => {
+    (async () => {
+      setSteps(selectedRound.id);
+    })();
+  }, [selectedRound.id]);
 
   return (
-    <header className="flex items-center h-16 px-10 bg-blue-200">
-      <div className="flex justify-between flex-1">
-        {stepsState.steps.map((step) => (
+    <header className="flex h-16 items-center justify-center bg-blue-200 px-10">
+      <StepsSectionContent />
+    </header>
+  );
+}
+
+export function StepsSectionContent() {
+  const stepsState = useStepsStore((state) => state);
+  const selectedRound = useSidebarStore((state) => state.selectedRound);
+
+  useEffect(() => {
+    (async () => {
+      stepsState.setSteps(selectedRound.id);
+    })();
+  }, [selectedRound.id]);
+
+  if (stepsState.loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (stepsState.error) {
+    return <p>{stepsState.error}</p>;
+  }
+
+  if (stepsState.steps.length === 0) {
+    return <p>No steps found</p>;
+  }
+
+  return (
+    <div className="flex flex-1 justify-between">
+      {stepsState.steps.length > 0 &&
+        stepsState.steps.map((step) => (
           <Button key={step.id} onClick={() => stepsState.setSelectedStep(step)}>
-            {step.name}
+            {/* <img src="step.icon" alt="" /> */}
+            {step.title}
           </Button>
         ))}
-      </div>
-    </header>
+    </div>
   );
 }
