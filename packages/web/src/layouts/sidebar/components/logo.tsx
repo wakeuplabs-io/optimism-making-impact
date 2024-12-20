f72117c63841bf79f34b44c232abdb7e0cffc69f
Nicolas <nicopuegher@gmail.com>
2024-12-20 16:41:30 -0300

    Reduce logo size on small screens

--- packages/web/src/layouts/sidebar/components/logo.tsx
+++ packages/web/src/layouts/sidebar/components/logo.tsx
@@ -2,11 +2,13 @@ import letterO from '@/layouts/sidebar/assets/logo/letter-o.png';
 import letterP from '@/layouts/sidebar/assets/logo/letter-p.png';
 
 export function Logo() {
+  const logoStyles = 'h-[22px] w-[23px] 2xl:h-[24px] 2xl:w-[25px]';
+
   return (
     <div className="flex h-6 gap-0.5">
-      <img src={letterO} alt="Letter O" />
-      <img src={letterP} alt="Letter P" />
-      <h1 className="mt-0.5">Impact</h1>
+      <img className={logoStyles} src={letterO} alt="Letter O" />
+      <img className={logoStyles} src={letterP} alt="Letter P" />
+      <h1 className="2xl:mt-[1px]">Impact</h1>
     </div>
   );
 }
