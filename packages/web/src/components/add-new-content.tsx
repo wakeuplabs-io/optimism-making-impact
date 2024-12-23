e86df0eb594a9b5922c89cf3cfafb50c506cdf2b
Nicolas <nicopuegher@gmail.com>
2024-12-23 13:52:14 -0300

    Create a reusable component to trigger new content for the backoffice

---
+++ packages/web/src/components/add-new-content.tsx
@@ -0,0 +1,18 @@
+import { Button } from '@/components/ui/button';
+import { Plus } from 'lucide-react';
+
+type AddNewContentProps = {
+  buttonText: string;
+  addNewContent: () => void;
+};
+export function AddNewContent(props: AddNewContentProps) {
+  return (
+    <Button
+      className="flex items-center justify-start rounded-xl bg-white-high px-2.5 py-5 shadow-none hover:bg-white-medium"
+      onClick={props.addNewContent}
+    >
+      <Plus strokeWidth={3} style={{ opacity: '30%' }} />
+      <span className="text-sm font-medium leading-5 text-dark-high 2xl:text-base">{props.buttonText}</span>
+    </Button>
+  );
+}
