6d0715c8f05f77e672f2123a1997b88df7bb6a8c
Nicolas <nicopuegher@gmail.com>
2024-12-23 13:54:02 -0300

    Update sidebar layout with new components

--- packages/web/src/layouts/sidebar.tsx
+++ packages/web/src/layouts/sidebar.tsx
@@ -1,17 +1,13 @@
-import { AddNewRound } from '@/features/sidebar/components/add-new-round';
 import { CategoryList } from '@/features/sidebar/components/category-list';
-import { DropdownMenu } from '@/features/sidebar/components/dropdown-menu';
 import { Logo } from '@/features/sidebar/components/logo';
+import { Rounds } from '@/features/sidebar/components/rounds';
 
 export function Sidebar() {
   return (
     <nav className="absolute h-full w-[320px] bg-white-high p-12 lg:static">
       <div className="flex h-full flex-col gap-6">
         <Logo />
-        <div className="grid gap-2">
-          <DropdownMenu />
-          <AddNewRound />
-        </div>
+        <Rounds />
         <CategoryList />
       </div>
     </nav>
