07c08642346eb8141d2ed9616cbcd3e33748a07e
Nicolas <nicopuegher@gmail.com>
2024-12-23 10:02:12 -0300

    Create an icon-button component on the same file as category-list

--- packages/web/src/features/sidebar/components/category-list.tsx
+++ packages/web/src/features/sidebar/components/category-list.tsx
@@ -1,3 +1,23 @@
+import { Button } from '@/components/ui/button';
+import { Blocks } from 'lucide-react';
+
 export function CategoryList() {
   return <ul>Category List</ul>;
+}
+
+type IconButtonProps = {
+  text: string;
+  isActive: boolean;
+};
+export function IconButton(props: IconButtonProps) {
+  return (
+    <Button
+      className={`flex w-full items-center justify-start rounded-xl px-2.5 py-5 shadow-none hover:text-dark-high ${
+        props.isActive ? 'bg-background text-dark-high hover:bg-background' : 'bg-white-high text-secondary hover:bg-white-medium'
+      }`}
+    >
+      <Blocks strokeWidth={1.7} style={{ width: '22px', height: '22px' }} />
+      <span className="text-sm font-semibold leading-5 2xl:text-base">{props.text}</span>
+    </Button>
+  );
 }
