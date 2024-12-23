35b684720fe6488708f0462adab5bafead44ba02
Nicolas <nicopuegher@gmail.com>
2024-12-23 18:22:28 -0300

    Create a form with a state to handle data submission on modals

---
+++ packages/web/src/features/sidebar/components/new-category-form.tsx
@@ -0,0 +1,69 @@
+import { IconButton } from '@/components/icon-button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { useSidebarStore } from '@/state';
+import { CategoryFormData } from '@/state';
+import { useState } from 'react';
+
+export function NewCategoryForm() {
+  const categoriesState = useSidebarStore((state) => state);
+
+  const [formData, setFormData] = useState<CategoryFormData>({
+    title: '',
+    iconURL: '',
+  });
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    categoriesState.addCategory(formData, categoriesState.selectedRound.id);
+
+    setFormData({ title: '', iconURL: '' });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="grid gap-4 py-4">
+        <div>
+          <Label htmlFor="title" className="sr-only">
+            Title
+          </Label>
+          <Input
+            id="title"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+            className="py-5 shadow-none placeholder:text-white-low focus-visible:ring-dark-low"
+            placeholder="Title"
+          />
+        </div>
+        <div>
+          <Label htmlFor="icon" className="sr-only">
+            Icon URL
+          </Label>
+          <Input
+            id="icon"
+            name="iconURL"
+            value={formData.iconURL}
+            onChange={handleChange}
+            className="py-5 shadow-none placeholder:text-white-low focus-visible:ring-dark-low"
+            placeholder="https://link-to-your-icon"
+          />
+        </div>
+      </div>
+      <div className="flex gap-2 pt-9">
+        <IconButton variant="save" />
+        <IconButton variant="delete" />
+      </div>
+    </form>
+  );
+}
