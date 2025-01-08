d9fb7227861b60f0811d46956006baa48ff970da
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-01-08 15:05:33 -0300

    Re add cors

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,6 +1,7 @@
 import envParsed from '@/envParsed.js';
 import middlewares from '@/middlewares/index.js';
 import routes from '@/routes/index.js';
+import cors from 'cors';
 import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
@@ -10,7 +11,7 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(helmet());
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', routes);
