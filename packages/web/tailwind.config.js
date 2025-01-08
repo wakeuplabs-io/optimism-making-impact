7ce4152de3cef48fa24a32a2a16b48f5a38d2456
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-01-08 13:47:32 -0300

    Change require for import

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 export default {
   darkMode: ['class'],
   content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
@@ -65,5 +67,5 @@ export default {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [tailwindcssAnimate],
 };
