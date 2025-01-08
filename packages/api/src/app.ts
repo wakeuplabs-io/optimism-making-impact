1bfdfb4f281df52fd680a3f11a2cada3afb52557
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-01-08 13:48:20 -0300

    Remove cors

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,19 +1,16 @@
-import cors from 'cors';
-import express from 'express';
-import helmet from 'helmet';
-import morgan from 'morgan';
-
-import serverless from 'serverless-http';
-
 import envParsed from '@/envParsed.js';
 import middlewares from '@/middlewares/index.js';
 import routes from '@/routes/index.js';
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import serverless from 'serverless-http';
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(helmet());
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 app.use('/api', routes);
