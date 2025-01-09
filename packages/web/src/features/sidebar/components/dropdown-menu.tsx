68c592cb396bd6b684273a9718773eb50b172793
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-01-09 10:10:35 -0300

    Adapt select component

--- packages/web/src/features/sidebar/components/dropdown-menu.tsx
+++ packages/web/src/features/sidebar/components/dropdown-menu.tsx
@@ -1,5 +1,6 @@
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { Select, SelectContent, SelectItem, SelectTrigger } from '@/components/ui/select';
 import { useSidebarStore } from '@/state';
+import { ChevronDown } from 'lucide-react';
 
 export function DropdownMenu() {
   const roundsState = useSidebarStore((state) => state);
@@ -8,7 +9,10 @@ export function DropdownMenu() {
     <nav>
       <Select onValueChange={(val) => roundsState.setSelectedRound(+val)}>
         <SelectTrigger className="w-[125px] border-none bg-transparent text-lg leading-6 text-dark-medium shadow-none focus:ring-0 2xl:text-xl">
-          <SelectValue placeholder={roundsState.selectedRound.name} />
+          <div className="flex items-center gap-8">
+            <p className="text-xl font-normal">{roundsState.selectedRound.name}</p>
+            <ChevronDown size={18} color="black" />
+          </div>
         </SelectTrigger>
         <SelectContent className="border-none bg-background">
           {roundsState.rounds.map((round) => (
