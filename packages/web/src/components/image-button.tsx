0064159d147d61ef82c1e4c78978cb54609659c9
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-01-10 15:16:23 -0300

    Created ImageButton component

--- packages/web/src/components/image-button.tsx
+++ packages/web/src/components/image-button.tsx
@@ -3,12 +3,15 @@ import { cn } from '@/lib/utils';
 interface ImageButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   src: string;
   className?: string;
+  isAdmin?: boolean;
+  editIcon?: React.ReactNode;
+  alt?: string;
 }
 
-export function ImageButton({ src, className, ...props }: ImageButtonProps) {
+export function ImageButton({ src, className, alt, ...props }: ImageButtonProps) {
   return (
-    <button className='' {...props}>
-      <img src={src} alt='' className={cn('transition-all duration-300 hover:brightness-75', className)} />
+    <button className='relative' {...props}>
+      <img src={src} alt={alt} className={cn('transition-all duration-300 hover:brightness-75', className)} />
     </button>
   );
 }
