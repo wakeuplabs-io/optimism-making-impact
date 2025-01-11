f42c6558585adf4008c58f71b4786a6be94d15ec
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-01-11 11:25:29 -0300

    Moved sidebar loading

--- packages/web/src/layout/sections/sidebar.tsx
+++ packages/web/src/layout/sections/sidebar.tsx
@@ -3,17 +3,48 @@ import { CategoryList } from '@/features/sidebar/components/category-list';
 import { Logo } from '@/features/sidebar/components/logo';
 import LogosSection from '@/features/sidebar/components/logos-section';
 import { Rounds } from '@/features/sidebar/components/rounds';
+import { useSidebarStore } from '@/state';
+import { useEffect } from 'react';
 
 export function SidebarSection() {
   return (
-    <nav className='absolute h-full w-[320px] bg-white-high p-6 lg:static'>
-      <div className='flex h-full flex-col gap-6'>
-        <Logo />
-        <Rounds />
-        <CategoryList />
-        <LogosSection />
-        <img src={WakeUpLogo} alt='' className='w-[124px]' />
-      </div>
-    </nav>
+    <SidebarContainer>
+      <SidebarContent />
+    </SidebarContainer>
+  );
+}
+
+interface SidebarContainerProps {
+  children: React.ReactNode;
+}
+
+function SidebarContainer(props: SidebarContainerProps) {
+  return <nav className='absolute h-full w-[320px] bg-white-high p-6 lg:static'>{props.children}</nav>;
+}
+
+function SidebarContent() {
+  const { setRounds, setCategories, loading } = useSidebarStore((state) => state);
+
+  useEffect(() => {
+    setRounds();
+    setCategories();
+  }, []);
+
+  if (loading) {
+    return (
+      <span>
+        <div className='flex flex-col items-center justify-center h-full gap-6'>Loading...</div>
+      </span>
+    );
+  }
+
+  return (
+    <div className='flex flex-col h-full gap-6'>
+      <Logo />
+      <Rounds />
+      <CategoryList />
+      <LogosSection />
+      <img src={WakeUpLogo} alt='' className='w-[124px]' />
+    </div>
   );
 }
