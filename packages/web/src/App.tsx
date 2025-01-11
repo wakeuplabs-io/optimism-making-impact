9ead8b63b79cc2de58005e709e86971ac4b77086
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-01-11 13:15:23 -0300

    Moved sidebar loading

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -2,33 +2,12 @@ import { IsAdminIndicator } from '@/layout/sections/is-admin-indicator';
 import { MainSection } from '@/layout/sections/main';
 import { SidebarSection } from '@/layout/sections/sidebar';
 import { StepsSection } from '@/layout/sections/steps';
-import { useSidebarStore } from '@/state';
-import { useEffect } from 'react';
 
 export function App() {
-  const fetchRounds = useSidebarStore((state) => state.setRounds);
-  const fetchCategories = useSidebarStore((state) => state.setCategories);
-  const loading = useSidebarStore((state) => state.loading);
-
-  useEffect(() => {
-    (async function setRounds() {
-      fetchRounds();
-    })();
-    fetchCategories();
-  }, [fetchRounds, fetchCategories]);
-
-  if (loading) {
-    return (
-      <div className="flex h-screen w-screen flex-col items-center justify-center lg:flex-row">
-        <div>Loading...</div>
-      </div>
-    );
-  }
-
   return (
-    <div className="flex h-screen w-screen flex-col lg:flex-row">
+    <div className='flex flex-col w-screen h-screen lg:flex-row'>
       <SidebarSection />
-      <div className="flex flex-1 flex-col">
+      <div className='flex flex-col flex-1'>
         <StepsSection />
         <MainSection />
       </div>
