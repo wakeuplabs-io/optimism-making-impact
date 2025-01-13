6d59009aa3731c0f7e3332326f204cb460889cec
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-01-13 08:12:41 -0300

    Make sidebar responsive

--- packages/web/src/layout/sections/sidebar.tsx
+++ packages/web/src/layout/sections/sidebar.tsx
@@ -1,14 +1,20 @@
 import WakeUpLogo from '@/assets/wake-up-logo.png';
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { MOBILE_THRESHOLD } from '@/config';
 import { CategoryList } from '@/features/sidebar/components/category-list';
 import { Logo } from '@/features/sidebar/components/logo';
 import LogosSection from '@/features/sidebar/components/logos-section';
 import { Rounds } from '@/features/sidebar/components/rounds';
 import { useSidebarStore } from '@/state';
+import { Menu } from 'lucide-react';
 import { useEffect } from 'react';
+import { useWindowSize } from 'usehooks-ts';
 
 export function SidebarSection() {
+  const title = useSidebarStore((state) => state.selectedRound.name);
+
   return (
-    <SidebarContainer>
+    <SidebarContainer title={title}>
       <SidebarContent />
     </SidebarContainer>
   );
@@ -16,9 +22,30 @@ export function SidebarSection() {
 
 interface SidebarContainerProps {
   children: React.ReactNode;
+  title?: string;
 }
 
 function SidebarContainer(props: SidebarContainerProps) {
+  const { width = 0 } = useWindowSize();
+  const isMobile = width < MOBILE_THRESHOLD;
+
+  if (isMobile) {
+    // Render as a Sheet on Mobile
+    return (
+      <nav className='flex h-28 w-full items-center justify-start gap-4 p-3 lg:static'>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Menu className='text-black' />
+          </SheetTrigger>
+
+          <SheetContent side='left'>{props.children}</SheetContent>
+        </Sheet>
+        <span>{props.title}</span>
+      </nav>
+    );
+  }
+
+  // Render static sidebar on Desktop
   return <nav className='absolute h-full w-[320px] bg-white-high p-6 lg:static'>{props.children}</nav>;
 }
 
@@ -32,19 +59,19 @@ function SidebarContent() {
 
   if (loading) {
     return (
-      <span>
-        <div className='flex flex-col items-center justify-center h-full gap-6'>Loading...</div>
-      </span>
+      <div className='flex h-full flex-col items-center justify-center gap-6'>
+        <span>Loading...</span>
+      </div>
     );
   }
 
   return (
-    <div className='flex flex-col h-full gap-6'>
+    <div className='flex h-full flex-col gap-6'>
       <Logo />
       <Rounds />
       <CategoryList />
       <LogosSection />
-      <img src={WakeUpLogo} alt='' className='w-[124px]' />
+      <img src={WakeUpLogo} alt='WakeUp Logo' className='w-[124px]' />
     </div>
   );
 }
