156fa999420515e8fce6617b53813ddd5e5aff16
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-01-13 07:18:36 -0300

    Add toast component

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from '@/components/ui/toaster';
 import { IsAdminIndicator } from '@/layout/sections/is-admin-indicator';
 import { MainSection } from '@/layout/sections/main';
 import { SidebarSection } from '@/layout/sections/sidebar';
@@ -5,12 +6,13 @@ import { StepsSection } from '@/layout/sections/steps';
 
 export function App() {
   return (
-    <div className='flex flex-col w-screen h-screen lg:flex-row'>
+    <div className='flex h-screen w-screen flex-col lg:flex-row'>
       <SidebarSection />
-      <div className='flex flex-col flex-1'>
+      <div className='flex flex-1 flex-col'>
         <StepsSection />
         <MainSection />
       </div>
+      <Toaster />
       {import.meta.env.DEV && <IsAdminIndicator />}
     </div>
   );
