88b851e19057e5f368e99643f769edc3bb645872
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-01-13 09:35:23 -0300

    Fix new category cancel button

--- packages/web/src/features/sidebar/components/new-category-form.tsx
+++ packages/web/src/features/sidebar/components/new-category-form.tsx
@@ -1,8 +1,9 @@
-import { IconButton } from '@/components/icon-button';
+import { ActionButton } from '@/components/action-button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { useSidebarStore } from '@/state';
-import { CategoryFormData } from '@/state';
+import { CategoryFormData, useSidebarStore } from '@/state';
+import { DialogClose } from '@radix-ui/react-dialog';
+import { Save } from 'lucide-react';
 import { useState } from 'react';
 
 export function NewCategoryForm() {
@@ -32,37 +33,41 @@ export function NewCategoryForm() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className="grid gap-4 py-4">
+      <div className='grid gap-4 py-4'>
         <div>
-          <Label htmlFor="title" className="sr-only">
+          <Label htmlFor='title' className='sr-only'>
             Title
           </Label>
           <Input
-            id="title"
-            name="title"
+            id='title'
+            name='title'
             value={formData.title}
             onChange={handleChange}
-            className="py-5 shadow-none placeholder:text-white-low focus-visible:ring-dark-low"
-            placeholder="Title"
+            className='py-5 shadow-none placeholder:text-white-low focus-visible:ring-dark-low'
+            placeholder='Title'
           />
         </div>
         <div>
-          <Label htmlFor="icon" className="sr-only">
+          <Label htmlFor='icon' className='sr-only'>
             Icon URL
           </Label>
           <Input
-            id="icon"
-            name="iconURL"
+            id='icon'
+            name='iconURL'
             value={formData.iconURL}
             onChange={handleChange}
-            className="py-5 shadow-none placeholder:text-white-low focus-visible:ring-dark-low"
-            placeholder="https://link-to-your-icon"
+            className='py-5 shadow-none placeholder:text-white-low focus-visible:ring-dark-low'
+            placeholder='https://link-to-your-icon'
           />
         </div>
       </div>
-      <div className="flex gap-2 pt-9">
-        <IconButton variant="save" />
-        <IconButton variant="delete" />
+      <div className='flex gap-2 pt-9'>
+        <DialogClose asChild>
+          <ActionButton label='Cancel' variant='secondary' />
+        </DialogClose>
+        <DialogClose asChild>
+          <ActionButton icon={<Save />} label='Save' variant='primary' type='submit' />
+        </DialogClose>
       </div>
     </form>
   );
