d83ce0e787c94e9bbfb59317be6ad4354ffb89e0
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-01-15 18:05:23 -0300

    Create steps with no type

--- packages/web/src/components/steps-button/types.ts
+++
@@ -1,6 +0,0 @@
-export type StepButtonState = 'past' | 'active' | 'coming';
-
-export interface StepButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode;
-  state: StepButtonState;
-}
--- packages/web/src/layout/sections/steps.tsx
+++ packages/web/src/layout/sections/steps.tsx
@@ -1,8 +1,15 @@
-import { IconWithDefault } from '@/components/icon-with-default';
-import { StepButton } from '@/components/steps-button';
-import { useSidebarStore } from '@/state';
+import { ActionButton } from '@/components/action-button';
+import { Modal } from '@/components/modal';
+import { StepButton } from '@/components/step-button';
+import { Input } from '@/components/ui/input';
+import { CreateStepBody } from '@/services/steps/schemas';
+import { useSidebarStore, useUserStore } from '@/state';
 import { useStepsStore } from '@/state/steps/steps-store';
-import { useEffect } from 'react';
+import { Step } from '@/types';
+import { DialogClose } from '@radix-ui/react-dialog';
+import { Label } from '@radix-ui/react-label';
+import { Plus, Save } from 'lucide-react';
+import { useEffect, useState } from 'react';
 
 export function StepsSection() {
   const setSteps = useStepsStore((state) => state.fetchByRoundId);
@@ -13,7 +20,7 @@ export function StepsSection() {
   }, [selectedRound.id]);
 
   return (
-    <header className='flex h-16 items-center justify-center px-10'>
+    <header className='flex items-center justify-center h-16 px-2'>
       <StepsSectionContent />
     </header>
   );
@@ -22,6 +29,7 @@ export function StepsSection() {
 export function StepsSectionContent() {
   const stepsState = useStepsStore((state) => state);
   const selectedRound = useSidebarStore((state) => state.selectedRound);
+  const isAdmin = useUserStore((state) => state.isAdmin);
 
   useEffect(() => {
     stepsState.fetchByRoundId(selectedRound.id);
@@ -36,29 +44,137 @@ export function StepsSectionContent() {
   }
 
   if (stepsState.steps.length === 0) {
-    return <p>No steps found</p>;
+    return <span>There are no steps for this round.</span>;
   }
 
   return (
-    <div className='flex max-w-full flex-1 justify-between overflow-hidden'>
-      {stepsState.steps.length > 0 &&
-        stepsState.steps.map((step) => {
-          const buttonState =
-            stepsState.selectedStep?.position === step.position
-              ? 'active'
-              : stepsState.selectedStep?.position && stepsState.selectedStep.position > step.position
-                ? 'past'
-                : 'coming';
-
-          return (
-            <StepButton state={buttonState} key={step.title} onClick={() => stepsState.setSelectedStep(step.id)}>
-              <div className='flex items-center gap-2'>
-                <IconWithDefault src={step.icon} />
-                {step.title}
-              </div>
-            </StepButton>
-          );
-        })}
+    <div className='flex items-center justify-between flex-1 max-w-full gap-4 overflow-hidden'>
+      <StepsList steps={stepsState.steps} />
+      {isAdmin && <AddStepModal roundId={selectedRound.id} onClick={stepsState.addStep} />}
     </div>
+  );
+}
+
+interface StepsListProps {
+  steps: Step[];
+}
+
+function StepsList(props: StepsListProps) {
+  const stepsState = useStepsStore((state) => state);
+  const isAdmin = useUserStore((state) => state.isAdmin);
+
+  return (
+    <div className='flex justify-between flex-1 max-w-full overflow-hidden'>
+      {props.steps.map((step) => {
+        const buttonState =
+          stepsState.selectedStep?.position === step.position
+            ? 'active'
+            : stepsState.selectedStep?.position && stepsState.selectedStep.position > step.position
+              ? 'past'
+              : 'coming';
+
+        return (
+          <StepButton
+            state={buttonState}
+            key={`${step.id}-${step.title}`}
+            onClick={() => stepsState.setSelectedStep(step.id)}
+            step={step}
+            isAdmin={isAdmin}
+            onDelete={stepsState.deleteStep}
+            onEdit={stepsState.editStep}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+function AddStepButton(props: React.HTMLAttributes<HTMLButtonElement>) {
+  return (
+    <button
+      className='flex h-[49px] w-[49px] items-center justify-center rounded-full bg-[#14141A] transition-all duration-500 ease-in-out hover:cursor-pointer hover:bg-primary'
+      {...props}
+    >
+      <Plus className='text-white' />
+    </button>
+  );
+}
+
+interface AddStepModalProps {
+  roundId: number;
+  onClick?: (roundId: number, data: CreateStepBody) => void;
+}
+
+function AddStepModal(props: AddStepModalProps) {
+  const [title, setTitle] = useState('');
+  const [icon, setIcon] = useState('');
+
+  function handleTitleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setTitle(event.target.value);
+  }
+
+  function handleIconChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setIcon(event.target.value);
+  }
+
+  function handleSubmit() {
+    props.onClick?.(props.roundId, { title, icon, roundId: props.roundId });
+    setTitle('');
+    setIcon('');
+  }
+
+  return (
+    <Modal title='Add step' trigger={<AddStepButton />}>
+      <div className='grid gap-4 py-4'>
+        <div>
+          <Label htmlFor='name' className='sr-only'>
+            Title
+          </Label>
+          <Input
+            id='name'
+            name='name'
+            value={title}
+            onChange={handleTitleChange}
+            className='py-5 shadow-none placeholder:text-white-low focus-visible:ring-dark-low'
+            placeholder='Title'
+          />
+        </div>
+        <div>
+          <Label htmlFor='icon' className='sr-only'>
+            Icon
+          </Label>
+          <Input
+            id='icon'
+            name='icon'
+            value={icon}
+            onChange={handleIconChange}
+            className='py-5 shadow-none placeholder:text-white-low focus-visible:ring-dark-low'
+            placeholder='Icon'
+          />
+        </div>
+        <div>
+          <Label htmlFor='type' className='sr-only'>
+            Type
+          </Label>
+          <Input
+            id='type'
+            name='type'
+            value={icon}
+            onChange={handleIconChange}
+            className='py-5 shadow-none placeholder:text-white-low focus-visible:ring-dark-low'
+            placeholder='Icon'
+          />
+        </div>
+      </div>
+
+      <div className='flex gap-4'>
+        <DialogClose asChild>
+          <ActionButton label='Cancel' variant='secondary' />
+        </DialogClose>
+        <DialogClose asChild>
+          <ActionButton icon={<Save />} label='Save' variant='primary' onClick={handleSubmit} />
+        </DialogClose>
+      </div>
+    </Modal>
   );
 }
