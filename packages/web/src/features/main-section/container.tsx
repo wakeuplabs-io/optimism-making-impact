4f4f044a8b4cef5a74d49edb8e7ef73f73c5ce34
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-01-16 13:13:13 -0300

    Make main container scrolleable in the y axis

--- packages/web/src/features/main-section/container.tsx
+++ packages/web/src/features/main-section/container.tsx
@@ -3,5 +3,5 @@ interface MainSectionContainerProps {
 }
 
 export function MainSectionContainer(props: MainSectionContainerProps) {
-  return <main className='flex flex-1 flex-col items-center justify-center gap-8 bg-red-100 p-4'>{props.children}</main>;
+  return <main className='flex flex-1 flex-col items-center justify-center gap-8 overflow-y-scroll bg-red-100 p-4'>{props.children}</main>;
 }
