3cfe746fc6da5c9a68ac58b45639955f54f551ea
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-01-17 09:42:20 -0300

    Create editable text component

--- packages/web/src/features/main-section/step-types/infographies/infography-card.tsx
+++ packages/web/src/features/main-section/step-types/infographies/infography-card.tsx
@@ -1,9 +1,11 @@
 import { ActionButton } from '@/components/action-button';
+import { EditableText } from '@/components/editable-text';
 import { Modal } from '@/components/modal';
 import { cn } from '@/lib/utils';
 import { Infography } from '@/types/infographies';
 import { DialogClose } from '@radix-ui/react-dialog';
 import { Trash, X } from 'lucide-react';
+import { useState } from 'react';
 
 interface InfogrpahyCardProps {
   infography: Infography;
@@ -13,18 +15,26 @@ interface InfogrpahyCardProps {
 }
 
 export function InfographyCard(props: InfogrpahyCardProps) {
+  const [markdown, setMarkdown] = useState(props.infography.markdown);
+
+  const handleTextareaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newMarkdown = e.target.value;
+    setMarkdown(newMarkdown);
+  };
+
   return (
     <div
       className={cn(
-        'relative flex w-full flex-col items-center justify-between gap-4 p-4 lg:flex-row lg:gap-24',
+        'relative flex w-full flex-col items-center justify-between gap-4 rounded-[10px] p-4 lg:flex-row lg:gap-24',
         props.order % 2 === 0 && 'lg:flex-row-reverse',
+        props.isAdmin && 'border border-[#BEBEBE]',
       )}
     >
       <div className='flex aspect-square max-h-[345px] w-full max-w-[345px] items-center justify-center'>
         <img className='h-full w-full rounded-full object-fill object-center' src={props.infography.image} alt='' />
       </div>
-      <div className='flex max-w-[500px] flex-[2] items-center'>
-        <span>{props.infography.markdown}</span>
+      <div className='flex w-screen max-w-[500px] flex-[2] items-center'>
+        <EditableText value={markdown} isAdmin={props.isAdmin} onChange={handleTextareaChange} />
       </div>
       {props.isAdmin && <DeleteIcon infography={props.infography} onClick={props.onDelete} />}
     </div>
@@ -39,7 +49,7 @@ interface DeleteIconProps {
 function DeleteIcon(props: DeleteIconProps) {
   return (
     <Modal
-      title='Delete step'
+      title='Delete infography'
       trigger={<X size={22} className='absolute right-5 top-5 cursor-pointer stroke-[#7D7D7D] hover:stroke-black' />}
     >
       <span>Are you sure you want to delete this infography?</span>
