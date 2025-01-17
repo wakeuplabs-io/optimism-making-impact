1df7cc2aa8a20fd31fcd07144d69907da6ddacb5
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-01-17 07:16:50 -0300

    Adjust infography card styles

--- packages/web/src/features/main-section/step-types/infographies/infography-card.tsx
+++ packages/web/src/features/main-section/step-types/infographies/infography-card.tsx
@@ -8,11 +8,16 @@ interface InfogrpahyCardProps {
 
 export function InfographyCard(props: InfogrpahyCardProps) {
   return (
-    <div className={cn('flex w-full flex-col justify-between gap-4 p-4 lg:flex-row', props.order % 2 === 0 && 'lg:flex-row-reverse')}>
-      <div className='flex h-[345px] w-[345px] items-center justify-center'>
+    <div
+      className={cn(
+        'flex w-full flex-col items-center justify-between gap-4 p-4 lg:flex-row lg:gap-24',
+        props.order % 2 === 0 && 'lg:flex-row-reverse',
+      )}
+    >
+      <div className='flex aspect-square max-h-[345px] w-full max-w-[345px] items-center justify-center'>
         <img className='h-full w-full rounded-full object-fill object-center' src={props.infography.image} alt='' />
       </div>
-      <div className='flex flex-[2] items-center'>
+      <div className='flex max-w-[500px] flex-[2] items-center'>
         <span>{props.infography.markdown}</span>
       </div>
     </div>
