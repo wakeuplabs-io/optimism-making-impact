19ec369cd58b3c755d29f64d53b75071bb54adad
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-01-20 13:37:28 -0300

    Autosave icon size

--- packages/web/src/components/auto-save-indicator.tsx
+++ packages/web/src/components/auto-save-indicator.tsx
@@ -1,4 +1,4 @@
-import AutosaveIcon from '@/components/icons/autosave-icon';
+import { AutosaveIcon } from '@/components/icons/autosave-icon';
 
 interface AutoSaveIndicatorProps {
   saving: boolean;
@@ -6,12 +6,13 @@ interface AutoSaveIndicatorProps {
 }
 
 export function AutoSaveIndicator(props: AutoSaveIndicatorProps) {
-  const text = props.saving ? 'Saving...' : props.pending ? 'Unsaved' : 'Autosaved';
+  // TODO: cuando se guarda que aparezca por un segundo autosaved y despues desaparezca (2 segundos)
+  const text = props.saving ? 'Saving...' : props.pending ? 'Unsaved' : '';
 
   return (
     <div className='flex items-center h-full gap-3'>
       <span className='text-[14px] text-[#9E9E9E]'>{text}</span>
-      <AutosaveIcon />
+      <AutosaveIcon size={45} />
     </div>
   );
 }
