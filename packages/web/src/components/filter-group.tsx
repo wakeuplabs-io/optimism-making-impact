5e7d6b55d540aed3a3f94713c9cac3cd349320b5
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-01-24 09:49:20 -0300

    Add tooltip to filter button

--- packages/web/src/components/filter-group.tsx
+++ packages/web/src/components/filter-group.tsx
@@ -1,6 +1,7 @@
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import { cn, getColor } from '@/lib/utils';
 import { Color } from '@/types';
-import { X } from 'lucide-react';
+import { Info, X } from 'lucide-react';
 import { ComponentProps } from 'react';
 
 interface Filter<T> {
@@ -10,6 +11,7 @@ interface Filter<T> {
   onClick?: (data: T) => void;
   prefixDot?: Color | undefined;
   editComponent?: React.ReactNode;
+  tooltipText?: string;
 }
 
 interface FilterGroupProps<T> {
@@ -34,6 +36,7 @@ export function FilterGroup<T>(props: FilterGroupProps<T>) {
             prefixDot={filter.prefixDot}
             isAdmin={props.isAdmin}
             editComponent={filter.editComponent}
+            tooltipText={filter.tooltipText}
           />
         ))}
       </div>
@@ -49,6 +52,7 @@ interface FilterButtonProps<T> extends Omit<ComponentProps<'button'>, 'onClick'>
   prefixDot?: Color | undefined;
   isAdmin?: boolean;
   editComponent?: React.ReactNode;
+  tooltipText?: string;
 }
 
 function FilterButton<T>({ label, selected, className, data, onClick, prefixDot, ...props }: FilterButtonProps<T>) {
@@ -78,9 +82,25 @@ function FilterButton<T>({ label, selected, className, data, onClick, prefixDot,
         >
           {label}
         </span>
+        {props.tooltipText && <InfoIcon tooltipText={props.tooltipText} />}
         {selected && <X size={12} className='stroke-[#4E4E4E] hover:stroke-black' />}
       </button>
       {props.isAdmin && props.editComponent}
     </div>
+  );
+}
+
+function InfoIcon(props: { tooltipText: string }) {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Info size={14} className='stroke-[#4E4E4E] hover:stroke-black' />
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>{props.tooltipText}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 }
