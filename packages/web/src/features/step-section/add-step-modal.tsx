a715f07de8f551863da0763e73231f287d0037d4
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-01-29 15:03:06 -0300

    Add step form

--- packages/web/src/features/step-section/add-step-modal.tsx
+++ packages/web/src/features/step-section/add-step-modal.tsx
@@ -1,20 +1,20 @@
-import { ActionButton } from '@/components/action-button';
-import { AddStepButton } from '@/components/add-step-button';
+import { IconButton } from '@/components/icon-button';
+import { SelectInput } from '@/components/inputs/select-input';
 import { Modal } from '@/components/modal';
-import { Select } from '@/components/select';
 import { Input } from '@/components/ui/input';
 import { capitalizeFirst } from '@/lib/utils';
+import { SmartListsService } from '@/services/smart-lists/service';
 import { CreateStepBody } from '@/services/steps/schemas';
 import { StepType, stepTypes } from '@/types';
-import { DialogClose } from '@radix-ui/react-dialog';
 import { Label } from '@radix-ui/react-label';
-import { Save } from 'lucide-react';
-import { useState } from 'react';
+import { Plus, Save } from 'lucide-react';
+import { useEffect, useState } from 'react';
 
 const options = stepTypes.map((type) => ({
   value: type,
   label: capitalizeFirst(type),
 }));
+const emptySmartListOption = { label: 'None', value: '0' };
 
 interface AddStepModalProps {
   roundId: number;
@@ -25,6 +25,22 @@ export function AddStepModal(props: AddStepModalProps) {
   const [title, setTitle] = useState('');
   const [icon, setIcon] = useState('');
   const [type, setType] = useState<string>(options[0].value);
+  const [smartListsOptions, setSmartListsOptions] = useState<{ label: string; value: string }[]>([]);
+  const [selectedSmartListId, setSelectedSmartListId] = useState(0);
+
+  useEffect(() => {
+    SmartListsService.getByRoundId(props.roundId).then((res) => {
+      const smartListsOptions = res.smartLists.map((smartList) => ({ label: smartList.title, value: smartList.id.toString() }));
+      setSmartListsOptions([emptySmartListOption, ...smartListsOptions]);
+    });
+  }, [props.roundId]);
+
+  function clear() {
+    setTitle('');
+    setIcon('');
+    setType(options[0].value);
+    setSelectedSmartListId(0);
+  }
 
   function handleTitleChange(event: React.ChangeEvent<HTMLInputElement>) {
     setTitle(event.target.value);
@@ -38,15 +54,35 @@ export function AddStepModal(props: AddStepModalProps) {
     setType(value);
   }
 
+  function handleSmartListChange(value: string) {
+    setSelectedSmartListId(+value);
+  }
+
   function handleSubmit() {
     const selectedType = type as StepType;
-    props.onClick?.(props.roundId, { title, icon, type: selectedType, roundId: props.roundId });
-    setTitle('');
-    setIcon('');
+
+    const shouldAddSmartList = type === 'CARDS' && selectedSmartListId !== 0;
+
+    props.onClick?.(props.roundId, {
+      title,
+      icon,
+      type: selectedType,
+      roundId: props.roundId,
+      smartListId: shouldAddSmartList ? selectedSmartListId : undefined,
+    });
+    clear();
   }
 
   return (
-    <Modal title='Add step' trigger={<AddStepButton />}>
+    <Modal
+      onOpenChange={(open) => !open && clear()}
+      title='Add step'
+      trigger={<IconButton variant='secondary' icon={<Plus className='text-white' />} />}
+      buttons={[
+        { label: 'Cancel', variant: 'secondary' },
+        { label: 'Save', variant: 'primary', disabled: false, icon: <Save />, onClick: handleSubmit },
+      ]}
+    >
       <div className='grid gap-4 py-4'>
         <div>
           <Label htmlFor='name' className='sr-only'>
@@ -74,21 +110,15 @@ export function AddStepModal(props: AddStepModalProps) {
             placeholder='Icon'
           />
         </div>
-        <div>
-          <Label htmlFor='type' className='sr-only'>
-            Type
-          </Label>
-          <Select items={options} placeholder='Select a type' defaultValue={options[0].value} onValueChange={handleTypeChange} />
-        </div>
-      </div>
-
-      <div className='flex gap-4'>
-        <DialogClose asChild>
-          <ActionButton label='Cancel' variant='secondary' />
-        </DialogClose>
-        <DialogClose asChild>
-          <ActionButton icon={<Save />} label='Save' variant='primary' onClick={handleSubmit} />
-        </DialogClose>
+        <SelectInput name='Type' items={options} onValueChange={handleTypeChange} value={type} />
+        {type === 'CARD' && (
+          <SelectInput
+            name='Smart List'
+            items={smartListsOptions}
+            onValueChange={handleSmartListChange}
+            value={selectedSmartListId.toString()}
+          />
+        )}
       </div>
     </Modal>
   );
