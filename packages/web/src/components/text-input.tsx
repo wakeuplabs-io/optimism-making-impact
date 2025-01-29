2ac47cf3aab60816b31e9be6b5e012e33420c387
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-01-29 15:19:07 -0300

    Simplify input

--- packages/web/src/components/text-input.tsx
+++ packages/web/src/components/text-input.tsx
@@ -1,22 +1,13 @@
 import { Input } from '@/components/ui/input';
 import { Label } from '@radix-ui/react-label';
 
-interface TextInputModalProps {
-  name: string;
-  value: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  placeholder?: string;
-}
-
-export function TextInput(props: TextInputModalProps) {
-  const { name, value, onChange, placeholder = 'Enter text' } = props;
-
+export function TextInput(props: React.ComponentProps<'input'>) {
   return (
     <div className='flex flex-col gap-1.5'>
-      <Label htmlFor={name} className='sr-only'>
-        <span>{name}</span>
+      <Label htmlFor={props.name} className='sr-only'>
+        <span>{props.name}</span>
       </Label>
-      <Input type='text' id={name} name={name} value={value} onChange={onChange} placeholder={placeholder} />
+      <Input {...props} />
     </div>
   );
 }
