df3eebf1fbcb19bb8ddd0b111276c4ac1a4052b9
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-01-29 15:51:45 -0300

    Fix add step modal

--- packages/web/src/features/step-section/add-step-modal.tsx
+++ packages/web/src/features/step-section/add-step-modal.tsx
@@ -7,7 +7,7 @@ import { SmartListsService } from '@/services/smart-lists/service';
 import { CreateStepBody } from '@/services/steps/schemas';
 import { StepType, stepTypes } from '@/types';
 import { Plus, Save } from 'lucide-react';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 const options = stepTypes.map((type) => ({
   value: type,
@@ -25,20 +25,24 @@ export function AddStepModal(props: AddStepModalProps) {
   const [icon, setIcon] = useState('');
   const [type, setType] = useState<string>(options[0].value);
   const [smartListsOptions, setSmartListsOptions] = useState<{ label: string; value: string }[]>([]);
-  const [selectedSmartListId, setSelectedSmartListId] = useState(0);
+  const [selectedSmartListId, setSelectedSmartListId] = useState<number | null>(null);
 
-  useEffect(() => {
-    SmartListsService.getByRoundId(props.roundId).then((res) => {
-      const smartListsOptions = res.smartLists.map((smartList) => ({ label: smartList.title, value: smartList.id.toString() }));
-      setSmartListsOptions([emptySmartListOption, ...smartListsOptions]);
-    });
-  }, [props.roundId]);
+  function handleOpenChange(open: boolean) {
+    if (!open) {
+      clear();
+    } else {
+      SmartListsService.getByRoundId(props.roundId).then((res) => {
+        const smartListsOptions = res.smartLists.map((smartList) => ({ label: smartList.title, value: smartList.id.toString() }));
+        setSmartListsOptions([emptySmartListOption, ...smartListsOptions]);
+      });
+    }
+  }
 
   function clear() {
     setTitle('');
     setIcon('');
     setType(options[0].value);
-    setSelectedSmartListId(0);
+    setSelectedSmartListId(null);
   }
 
   function handleTitleChange(event: React.ChangeEvent<HTMLInputElement>) {
@@ -60,7 +64,7 @@ export function AddStepModal(props: AddStepModalProps) {
   function handleSubmit() {
     const selectedType = type as StepType;
 
-    const shouldAddSmartList = type === 'CARDS' && selectedSmartListId !== 0;
+    const shouldAddSmartList = type === 'CARD' && selectedSmartListId !== null && selectedSmartListId !== 0;
 
     props.onClick?.(props.roundId, {
       title,
@@ -72,14 +76,16 @@ export function AddStepModal(props: AddStepModalProps) {
     clear();
   }
 
+  const saveDisabled = title === '' || (type === 'CARD' && selectedSmartListId === null);
+
   return (
     <Modal
-      onOpenChange={(open) => !open && clear()}
+      onOpenChange={handleOpenChange}
       title='Add step'
       trigger={<IconButton variant='secondary' icon={<Plus className='text-white' />} />}
       buttons={[
         { label: 'Cancel', variant: 'secondary' },
-        { label: 'Save', variant: 'primary', disabled: false, icon: <Save />, onClick: handleSubmit },
+        { label: 'Save', variant: 'primary', disabled: saveDisabled, icon: <Save />, onClick: handleSubmit },
       ]}
     >
       <div className='flex flex-col gap-4'>
@@ -87,12 +93,7 @@ export function AddStepModal(props: AddStepModalProps) {
         <TextInput name='Icon' placeholder='Icon' value={icon} onChange={handleIconChange} />
         <SelectInput name='Type' items={options} onValueChange={handleTypeChange} value={type} />
         {type === 'CARD' && (
-          <SelectInput
-            name='Smart List'
-            items={smartListsOptions}
-            onValueChange={handleSmartListChange}
-            value={selectedSmartListId.toString()}
-          />
+          <SelectInput name='Smart List' items={smartListsOptions} onValueChange={handleSmartListChange} placeholder='Select Smart List' />
         )}
       </div>
     </Modal>
