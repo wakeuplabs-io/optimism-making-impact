2f8364ed6e01014a0f2ca03a17ee88f551d9c911
Matias Barro <matias.barro@gmail.com>
2025-02-06 17:10:54 -0300

    Add UserWhitelist prisma schema

---
+++ packages/api/prisma/seeding/user-whitelist.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client';
+
+const userWhiteListData = [{ email: 'matias.barro@gmail.com' }];
+
+export async function seedUserWhitelist(prisma: PrismaClient) {
+  console.log('Seeding user whitelist...');
+
+  const { count } = await prisma.userWhitelist.createMany({
+    data: userWhiteListData,
+    skipDuplicates: true, // Avoid errors if the script is run multiple times
+  });
+
+  console.log(`${count} users seeded successfully!`);
+
+  // Return created users for confirmation (optional)
+  return prisma.userWhitelist.findMany();
+}
