59f181a83aaf0131f2a7e809bd076a0e1ccfecb9
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-02-07 09:08:54 -0300

    Fix round helpers

--- packages/api/src/lib/prisma/helpers.ts
+++ packages/api/src/lib/prisma/helpers.ts
@@ -2,13 +2,13 @@ import { prisma } from '@/lib/prisma/instance.js';
 import { CompleteRound } from '@/types/entities/round.js';
 
 /**
- * Fetches a complete round from the database, including all associated categories, steps, smart lists, infographics, items, cards, attributes, and keywords.
+ * Fetches a complete round from the database, including all associated categories, steps, smart lists, infographics, items, cards, attributes, and keywords, if exists.
  * @param {number} roundId - The ID of the round to fetch.
  * @returns {Promise<CompleteRound>} A promise that resolves to the fetched round.
  * @throws {Prisma.PrismaClientKnownRequestError} If the round does not exist.
  */
-export function getCompleteRound(roundId: number): Promise<CompleteRound> {
-  return prisma.round.findUniqueOrThrow({
+export function getCompleteRound(roundId: number): Promise<CompleteRound | null> {
+  return prisma.round.findUnique({
     where: { id: roundId },
     include: {
       categories: {
@@ -35,8 +35,10 @@ export function getCompleteRound(roundId: number): Promise<CompleteRound> {
  * @returns The most recent round.
  * @throws If no round exists.
  */
-export async function getLastCompleteRound(): Promise<CompleteRound> {
-  const lastRoundId = await prisma.round.findFirstOrThrow({ orderBy: { id: 'desc' }, select: { id: true } });
+export async function getLastCompleteRound(): Promise<CompleteRound | null> {
+  const lastRoundId = await prisma.round.findFirst({ orderBy: { id: 'desc' }, select: { id: true } });
+
+  if (!lastRoundId) return null;
 
   return getCompleteRound(lastRoundId.id);
 }
