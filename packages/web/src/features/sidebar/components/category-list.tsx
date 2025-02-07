101a2f741953fe529f8526339fd811cf44e9fe70
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-02-07 09:24:04 -0300

    Re arrange sidebar for empty content

--- packages/web/src/features/sidebar/components/category-list.tsx
+++ packages/web/src/features/sidebar/components/category-list.tsx
@@ -1,27 +1,38 @@
 import { CategoryButton } from '@/components/category-button';
 import { useSidebarStore, useUserStore } from '@/state';
+import { Category } from '@/types';
 
-export function CategoryList() {
+export function CategoryList(props: ContentProps) {
+  return (
+    <Container>
+      <Content {...props} />
+    </Container>
+  );
+}
+
+function Container(props: { children: React.ReactNode }) {
+  return <div className='flex flex-col gap-2 my-5'>{props.children} </div>;
+}
+
+interface ContentProps {
+  categories: Category[];
+}
+
+function Content(props: ContentProps) {
   const sidebarState = useSidebarStore((state) => state);
   const isAdmin = useUserStore((state) => state.user.isAdmin);
 
-  function handleCategoryClick(categoryId: number) {
-    sidebarState.setSelectedCategoryId(categoryId);
-  }
-
-  if (!sidebarState.selectedRound?.categories.length) {
-    return <p className='text-sm text-primary'>There are no categories yet.</p>;
-  }
+  if (props.categories.length === 0) return <EmptyState />;
 
   return (
     <>
       <ul className='flex flex-col gap-2'>
-        {sidebarState.selectedRound.categories.map((category) => (
+        {props.categories.map((category) => (
           <li key={category.id}>
             <CategoryButton
               category={category}
               isActive={sidebarState.selectedCategoryId === category.id}
-              onClick={() => handleCategoryClick(category.id)}
+              onClick={() => sidebarState.setSelectedCategoryId(category.id)}
               isAdmin={isAdmin}
               onDelete={sidebarState.deleteCategory}
               onEdit={sidebarState.editCategory}
@@ -31,4 +42,8 @@ export function CategoryList() {
       </ul>
     </>
   );
+}
+
+function EmptyState() {
+  return <p className='text-sm text-center'>There are no categories yet.</p>;
 }
--- packages/web/src/features/sidebar/components/rounds.tsx
+++
@@ -1,9 +0,0 @@
-import { RoundsSelect } from '@/features/sidebar/components/dropdown-menu';
-
-export function Rounds() {
-  return (
-    <div className='grid gap-2'>
-      <RoundsSelect />
-    </div>
-  );
-}
