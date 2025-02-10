71f59a979388017229e4dc5ca144b9a930b9be2e
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-02-10 10:05:41 -0300

    Remove unnecessary color

--- packages/web/src/layout/sections/steps.tsx
+++ packages/web/src/layout/sections/steps.tsx
@@ -2,7 +2,7 @@ import { StepsSectionContent } from '@/features/step-section';
 
 export function StepsSection() {
   return (
-    <header className='flex h-16 items-center justify-center bg-[#F1F4F9] px-2'>
+    <header className='flex items-center justify-center h-16 px-2'>
       <StepsSectionContent />
     </header>
   );
