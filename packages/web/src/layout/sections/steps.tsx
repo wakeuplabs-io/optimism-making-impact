313a425bc2b221c546c045e43b41c1f1355760dc
Matias Barro <matias.barro@gmail.com>
2025-02-10 18:18:16 -0300

    Add padding top and bottom in headers

--- packages/web/src/layout/sections/steps.tsx
+++ packages/web/src/layout/sections/steps.tsx
@@ -2,7 +2,7 @@ import { StepsSectionContent } from '@/features/step-section';
 
 export function StepsSection() {
   return (
-    <header className='flex items-center justify-center px-4 h-16 '>
+    <header className='flex items-center justify-center px-4 h-16 md:py-16'>
       <StepsSectionContent />
     </header>
   );
