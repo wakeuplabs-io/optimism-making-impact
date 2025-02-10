14bbb3c31ef8636e6042b22b44cec767820ccc9f
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-02-10 17:57:33 -0300

    Adjust cards design

--- packages/web/src/features/main-section/step-types/items/index.tsx
+++ packages/web/src/features/main-section/step-types/items/index.tsx
@@ -1,6 +1,5 @@
 import { ItemFilters } from '@/features/main-section/step-types/items/filters-list';
 import { ItemsList } from '@/features/main-section/step-types/items/items-list';
-import { cn } from '@/lib/utils';
 import { CompleteStep } from '@/types';
 
 interface ItemStepProps {
@@ -9,15 +8,17 @@ interface ItemStepProps {
 
 export function ItemsStep(props: ItemStepProps) {
   return (
-    <div className={cn('flex h-full w-full flex-col gap-4')}>
-      <div className='flex flex-col lg:flex-row'>
+    <div className={'flex h-full w-full flex-col gap-4'}>
+      <div className='flex h-full flex-col gap-6 lg:flex-row'>
         <ItemFilters smartList={props.step.smartList} stepId={props.step.id} />
-        <ItemsList
-          items={props.step.items}
-          stepId={props.step.id}
-          title={props.step.description}
-          attributes={props.step.smartList?.attributes}
-        />
+        <div className='w-full overflow-y-auto pb-8'>
+          <ItemsList
+            items={props.step.items}
+            stepId={props.step.id}
+            title={props.step.description}
+            attributes={props.step.smartList?.attributes}
+          />
+        </div>
       </div>
     </div>
   );
