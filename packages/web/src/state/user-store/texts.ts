4b0a3da99b5f907ffe25ec040169256cf6599184
Matias Barro <matias.barro@gmail.com>
2025-02-10 12:41:41 -0300

    Logout non admin users

---
+++ packages/web/src/state/user-store/texts.ts
@@ -0,0 +1,5 @@
+export const AUTHENTICATION_ERROR_TITLE = 'Authentication Error';
+export const AUTHENTICATION_ERROR_DESCRIPTION = 'Failed to authenticate your user. Signing out.';
+export const AUTHENTICATION_NOT_ADMIN_ERROR_DESCRIPTION = 'You re not authorized to access this page.';
+export const AUTHENTICATION_SUCCESS_TITLE = 'Welcome!';
+export const AUTHENTICATION_SUCCESS_DESCRIPTION = 'You have access to edit the site.';
