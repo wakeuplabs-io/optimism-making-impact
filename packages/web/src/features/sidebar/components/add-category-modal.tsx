025b6cf901af61e70ab2479f5b3185d301e622af
francoperez03 <franco.perez03@gmail.com>
2025-02-20 14:52:05 -0300

    refactor: improve coding

--- packages/web/src/features/sidebar/components/add-category-modal.tsx
+++ packages/web/src/features/sidebar/components/add-category-modal.tsx
@@ -1,13 +1,12 @@
-import { Controller, useFormContext } from 'react-hook-form';
-import { FormModal } from '@/components/form-modal';
-import { NewButton } from '@/components/new-button';
-import { Plus } from 'lucide-react';
 import { IconPicker } from './icon-picker';
-import { createElement, useState } from 'react';
-import * as LucideIcons from 'lucide-react';
-import { SidebarActionButton } from '@/components/sidebar-acion-button';
-import { CreateCategoryBody, createCategoryBodySchema } from '@optimism-making-impact/schemas';
+import { FormModal } from '@/components/form-modal';
 import { FormTextInput } from '@/components/form/form-text-input';
+import { NewButton } from '@/components/new-button';
+import { CreateCategoryBody, createCategoryBodySchema } from '@optimism-making-impact/schemas';
+import { Plus } from 'lucide-react';
+import * as LucideIcons from 'lucide-react';
+import { createElement } from 'react';
+import { Controller, useFormContext } from 'react-hook-form';
 
 interface AddCategoryModalProps {
   roundId: number;
@@ -15,12 +14,11 @@ interface AddCategoryModalProps {
 }
 
 const modalIcons: Record<string, React.ComponentType> = Object.fromEntries(
-  Object.entries(LucideIcons)
-    .map(([key, value]) => [key.toLowerCase(), value as React.ComponentType])
+  Object.entries(LucideIcons).map(([key, value]) => [key.toLowerCase(), value as React.ComponentType]),
 );
 
 export function AddCategoryModal(props: AddCategoryModalProps) {
-  const defaultValues: CreateCategoryBody = { name: '', icon: 'dot', roundId: props.roundId };
+  const defaultValues: CreateCategoryBody = { name: '', icon: '', roundId: props.roundId };
 
   function handleSubmit(data: CreateCategoryBody) {
     props.onSave?.(data.name, data.icon, props.roundId);
@@ -45,70 +43,45 @@ interface FormFieldsProps {
 
 function FormFields(props: FormFieldsProps) {
   const { control, setValue, watch } = useFormContext<CreateCategoryBody>();
-
-  // return (
-  //   <div className="grid grid-cols-1 gap-2 w-full">
-  //     <div className="grid grid-cols-[50px_1fr] gap-2 items-center text-gray-500 text-sm">
-  //       <span>Icon</span>
-  //       <span>Title</span>
-  //     </div>
-
-  //     <div className="grid grid-cols-[50px_1fr] gap-2 items-center">
-  //       <Controller
-  //         name='iconUrl'
-  //         control={control}
-  //         defaultValue={props.defaultValues.iconUrl}
-  //         render={({ field }) => (
-
-  //         )}
-  //       />
-
-  //       <Controller
-  //         name='name'
-  //         control={control}
-  //         defaultValue={props.defaultValues.name}
-  //         render={({ field }) => 
-  //           <input
-  //             {...field}
-  //             type="text"
-  //             placeholder="Write here..."
-  //             className="w-full h-[42px] rounded-md border border-gray-300 px-3 text-sm focus:border-red-500 focus:ring-0"
-  //           />
-  //         }
-  //       />
-  //     </div>
   const selectedIcon = watch('icon');
+
   return (
-    <div className="grid grid-cols-[50px_1fr] gap-2 items-center">
+    <div className='grid grid-cols-[50px_1fr] items-center gap-2'>
       <div
-          onClick={() => console.log("Open icon picker")}
-          className="flex h-[42px] w-[42px] items-center justify-center rounded-md border border-gray-300 cursor-pointer"
-        >
-      {modalIcons[selectedIcon] ? (
-        createElement(modalIcons[selectedIcon])
-      ) : (
-        <Plus className="text-red-500 w-6 h-6" />
-      )}
+        onClick={() => console.log('Open icon picker')}
+        className='flex h-[42px] w-[42px] cursor-pointer items-center justify-center rounded-md border border-gray-300'
+      >
+        {modalIcons[selectedIcon] ? createElement(modalIcons[selectedIcon]) : <Plus className='h-6 w-6 text-red-500' />}
       </div>
       <Controller
         name='name'
         control={control}
         defaultValue={props.defaultValues.name}
-        render={({ field, fieldState }) => <FormTextInput {...field} className="w-full h-[42px] rounded-md border border-gray-300 px-3 text-sm focus:border-red-500 focus:ring-0" error={fieldState.error?.message} placeholder='Name' />}
+        render={({ field, fieldState }) => (
+          <FormTextInput
+            {...field}
+            className='h-[42px] w-full rounded-md border border-gray-300 px-3 text-sm focus:border-red-500 focus:ring-0'
+            error={fieldState.error?.message}
+            placeholder='Name'
+          />
+        )}
       />
       <Controller
         name='icon'
         control={control}
         defaultValue={props.defaultValues.icon}
-        render={({ field }) =>
-          <IconPicker 
-            selectedIcon={field.value} 
+        render={({ field, fieldState }) => (
+          <>
+          <IconPicker
+            selectedIcon={field.value}
             modalIcons={modalIcons}
             onSelect={(icon) => {
               setValue('icon', icon);
-            }} 
+            }}
           />
-        }
+          <p className='text-sm text-gray-500'>{fieldState.error?.message}</p>
+          </>
+        )}
       />
     </div>
   );
--- packages/web/src/features/sidebar/components/category-list.tsx
+++ packages/web/src/features/sidebar/components/category-list.tsx
@@ -49,7 +49,6 @@ function Content({ title, categories }: ContentProps) {
                 <AddCategoryModal roundId={1} onSave={() => {}} />
               </li>
             )}
-            ,
           </>
         )}
       </ul>
