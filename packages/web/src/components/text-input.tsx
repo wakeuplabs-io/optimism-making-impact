6319d99d2fea21e252950971f07c32dd1dbecd6c
Matias Barro <matias.barro@gmail.com>
2025-02-24 16:42:31 -0300

    Update label style of text-input component

--- packages/web/src/components/text-input.tsx
+++ packages/web/src/components/text-input.tsx
@@ -4,8 +4,8 @@ import { Label } from '@radix-ui/react-label';
 export function TextInput(props: React.ComponentProps<'input'>) {
   return (
     <div className='flex flex-col gap-1.5'>
-      <Label htmlFor={props.name} className='sr-only'>
-        <span>{props.name}</span>
+      <Label htmlFor={props.name} className='text-xs text-[#BEBEBE] font-normal'>
+        <span className='capitalize'>{props.name}</span>
       </Label>
       <Input {...props} />
     </div>
