c2af75d764a831fbebf7bc47cd03720e3613be1f
Luciano Pérez Cerra <perezcerraluciano@gmail.com>
2025-02-25 15:58:43 -0300

    add logs for debugging

--- sst.config.ts
+++ sst.config.ts
@@ -81,6 +81,7 @@ export default $config({
         },
       ],
     });
+    console.log('ENVS!!!!', process.env.DB_URL, process.env.DB_URL_NON_POOLING);
 
     const apiGateway = new sst.aws.ApiGatewayV2('api', {
       cors: true,
