1d3c7d809f925868bf600fc6c61f3d9a63a8d710
Matias Barro <matias.barro@gmail.com>
2025-03-10 15:48:06 -0300

    Update delete infographic button style

--- packages/web/src/features/main-section/step-types/infographics/delete-infographic-modal.tsx
+++ packages/web/src/features/main-section/step-types/infographics/delete-infographic-modal.tsx
@@ -1,6 +1,6 @@
 import { Modal } from '@/components/modal';
 import { Infographic } from '@/types/infographics';
-import { Trash, X } from 'lucide-react';
+import { Trash } from 'lucide-react';
 
 interface DeleteInfographicModalProps {
   infographic: Infographic;
@@ -11,7 +11,7 @@ export function DeleteInfographicModal(props: DeleteInfographicModalProps) {
   return (
     <Modal
       title='Delete infographic'
-      trigger={<X size={22} className='cursor-pointer stroke-[#7D7D7D] hover:stroke-black' />}
+      trigger={<Trash className='w-[24px] h-[24px] cursor-pointer stroke-[#7D7D7D] hover:stroke-black' />}
       buttons={[
         { label: 'Cancel', variant: 'secondary', closeOnClick: true },
         { label: 'Delete', variant: 'primary', icon: <Trash />, onClick: () => props.onClick?.(props.infographic.id) },
