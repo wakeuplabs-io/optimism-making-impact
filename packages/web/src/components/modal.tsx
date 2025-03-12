2d6df3e11ba7fc5330aa24bf93e0c1c5d48deb44
Matias Barro <matias.barro@gmail.com>
2025-03-12 09:06:07 -0300

    Update modal header

--- packages/web/src/components/modal.tsx
+++ packages/web/src/components/modal.tsx
@@ -8,8 +8,9 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from '@/components/ui/dialog';
+} from '@/components/ui/dialog-full-screen-mobile';
 import { DialogProps } from '@radix-ui/react-dialog';
+import { X } from 'lucide-react';
 import React from 'react';
 
 interface ModalProps extends DialogProps {
@@ -31,10 +32,16 @@ export function Modal({ buttons = [], contentProps = {}, ...props }: ModalProps)
   return (
     <Dialog {...props}>
       <DialogTrigger>{props.trigger}</DialogTrigger>
-      <DialogContent className='flex w-fit min-w-[95%] sm:min-w-0 max-w-[95%] flex-col items-center rounded-[22px] p-6' {...contentProps}>
-        <DialogHeader className='flex w-full items-center justify-center'>
-          {props.title && <DialogTitle className='place-self-start text-lg text-dark-low 2xl:text-xl'>{props.title}</DialogTitle>}
-          {props.subtitle && <DialogDescription className='text-center text-secondary'>{props.subtitle}</DialogDescription>}
+      <DialogContent className='flex! flex-col lg:max-w-[95%] rounded-[22px] p-6 md:p-12' hideCloseButton={true} {...contentProps}>
+        <DialogHeader className='flex flex-row items-center justify-between'>
+          <div className='flex flex-col items-start justify-between'>
+            {props.title && <DialogTitle className='text-xl text-dark-high font-medium'>{props.title}</DialogTitle>}
+            {props.subtitle && <DialogDescription className='text-secondary'>{props.subtitle}</DialogDescription>}
+          </div>
+          <DialogClose>
+            <X className='h-6 w-6' />
+            <span className='sr-only'>Close</span>
+          </DialogClose>
         </DialogHeader>
         <div className='w-full'>{props.children}</div>
         {buttons.length > 0 && (
