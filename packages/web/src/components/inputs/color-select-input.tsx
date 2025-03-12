5c9d6d0ec9d28aeb0c145cb94c613c1f8d8b22b6
Martin <59508264+martinc1991@users.noreply.github.com>
2025-03-12 14:01:24 -0300

    Fix: remove context in favor of local state for color select input (#107)

--- packages/web/src/components/inputs/color-select-input.tsx
+++ packages/web/src/components/inputs/color-select-input.tsx
@@ -1,6 +1,6 @@
 import { cn, getColor } from '@/lib/utils';
 import { Color, colorList } from '@optimism-making-impact/schemas';
-import { createContext, useContext, useState } from 'react';
+import { useState } from 'react';
 
 interface ColorSelectInputProps {
   onChange?: (color: Color) => void;
@@ -18,20 +18,18 @@ export function ColorSelectInput(props: ColorSelectInputProps) {
   };
 
   return (
-    <ColorContext.Provider value={{ selectedColor, setSelectedColor }}>
-      <div
-        className={cn(
-          'border-1 grid min-h-8 w-fit gap-3 rounded-md border border-input px-2 py-1',
-          'grid-cols-5' /* Default to 5 columns */,
-          'md:grid-cols-10' /* Use 10 columns for larger screens */,
-          props.containerClassName,
-        )}
-      >
-        {colorList.map((color) => (
-          <ColorOption key={color} color={color} onClick={handleColorChange} disabled={props.disabled} />
-        ))}
-      </div>
-    </ColorContext.Provider>
+    <div
+      className={cn(
+        'border-1 grid min-h-8 w-fit gap-3 rounded-md border border-input px-2 py-1',
+        'grid-cols-5',
+        'md:grid-cols-10',
+        props.containerClassName,
+      )}
+    >
+      {colorList.map((color) => (
+        <ColorOption key={color} color={color} onClick={handleColorChange} disabled={props.disabled} isSelected={selectedColor === color} />
+      ))}
+    </div>
   );
 }
 
@@ -39,13 +37,10 @@ interface ColorOptionProps {
   color: Color;
   onClick?: (color: Color) => void;
   disabled?: boolean;
+  isSelected: boolean;
 }
 
 function ColorOption(props: ColorOptionProps) {
-  const { selectedColor } = useColorContext();
-
-  const isSelected = selectedColor === props.color;
-
   return (
     <button
       className={cn('flex h-[23px] w-[23px] items-center justify-center rounded-full', props.disabled && 'opacity-50')}
@@ -56,23 +51,7 @@ function ColorOption(props: ColorOptionProps) {
       }}
       disabled={props.disabled}
     >
-      <div className={cn('h-[19px] w-[19px] rounded-full', isSelected && 'border-2 border-white')} />
+      <div className={cn('h-[19px] w-[19px] rounded-full', props.isSelected && 'border-2 border-white')} />
     </button>
   );
 }
-
-// Context
-interface ColorContextType {
-  selectedColor: Color | null;
-  setSelectedColor: (color: Color) => void;
-}
-
-const ColorContext = createContext<ColorContextType | undefined>(undefined);
-
-const useColorContext = (): ColorContextType => {
-  const context = useContext(ColorContext);
-  if (!context) {
-    throw new Error('useColorContext must be used within a ColorProvider');
-  }
-  return context;
-};
