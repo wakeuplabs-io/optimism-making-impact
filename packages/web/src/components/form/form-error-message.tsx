2ab839ca068f18ad4b4ed15f51519bd6391e3604
Matias Barro <matias.barro@gmail.com>
2025-03-13 11:28:35 -0300

    Add comment explaining why form error message is invisible instead of unmounted when no error is provided

--- packages/web/src/components/form/form-error-message.tsx
+++ packages/web/src/components/form/form-error-message.tsx
@@ -11,6 +11,8 @@ export function FormErrorMessage({ error, className }: FormErrorMessageProps) {
       className={cn(
         'text-xs text-red-500 min-h-[20px]',
         {
+          // Error component will always be rendered, but invisible if no error.
+          // This way we avoid the modal resizing when the error message is shown.
           invisible: !error,
         },
         className,
