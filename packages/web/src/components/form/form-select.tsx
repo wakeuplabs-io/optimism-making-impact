74daeeb10bf2f76801ac51b1d07c8c538b14ff8d
Matias Barro <matias.barro@gmail.com>
2025-03-13 18:28:56 -0300

    Add form-select component

---
+++ packages/web/src/components/form/form-select.tsx
@@ -0,0 +1,23 @@
+import { cn } from '@/lib/utils';
+import { SelectInput } from '../inputs/select-input';
+import { FormInputWrapper } from './form-input';
+
+interface FormSelectProps extends React.ComponentProps<typeof SelectInput> {
+  error?: string;
+}
+
+export function FormSelect({ error, triggerClassName, ...props }: FormSelectProps) {
+  return (
+    <FormInputWrapper error={error}>
+      <SelectInput
+        triggerClassName={cn(
+          {
+            'border-red-500 focus-visible:ring-red-500': !!error,
+          },
+          triggerClassName,
+        )}
+        {...props}
+      />
+    </FormInputWrapper>
+  );
+}
