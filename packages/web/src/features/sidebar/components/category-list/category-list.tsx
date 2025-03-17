6ddabcdc3f1eb1dc04f088d4d6899da88b02bee2
Luciano Pérez Cerra <perezcerraluciano@gmail.com>
2025-03-17 09:45:51 -0300

    complete roundlist component with new approach using react-query

--- packages/web/src/features/sidebar/components/category-list/category-list.tsx
+++ packages/web/src/features/sidebar/components/category-list/category-list.tsx
@@ -1,15 +1,19 @@
 import { AddCategoryModal } from '../add-category-modal';
 import { SidebarSectionList } from '../sidebar-section-list';
 import { CategoryListButton } from '@/features/sidebar/components/category-list/category-list-button';
-import { useSidebarStore } from '@/state/sidebar/sidebar-store';
+import { router } from '@/router';
+import { CategoriesService } from '@/services/categories-service';
+import { RoundsService } from '@/services/rounds-service';
 import { useUserStore } from '@/state/user-store/user-store';
 import { Category } from '@/types/categories';
-import { useState } from 'react';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { useSearch } from '@tanstack/react-router';
+import { useEffect, useState } from 'react';
 
-export function CategoryList(props: ContentProps) {
+export function CategoryList() {
   return (
     <Container>
-      <Content {...props} />
+      <Content />
     </Container>
   );
 }
@@ -18,47 +22,86 @@ function Container(props: { children: React.ReactNode }) {
   return <div className='flex flex-col gap-2'>{props.children} </div>;
 }
 
-interface ContentProps {
-  roundId?: number;
-  categories?: Category[];
-}
-
-function Content({ categories, roundId }: ContentProps) {
-  const sidebarState = useSidebarStore((state) => state);
+function Content() {
   const isAdmin = useUserStore((state) => state.isAdminModeEnabled);
-  const addCategory = useSidebarStore((state) => state.addCategory);
 
-  // This loading actually depends on round fetching
-  // If there's no selected round then there are no categories to show
-  const [isLoading, setIsLoading] = useState(true);
-  setTimeout(() => {
-    setIsLoading(false);
-  }, 2000);
+  const search = useSearch({ from: '/' });
+  const { roundId } = search;
+  const { data: rounds = [], isLoading: roundsLoading } = useQuery({
+    queryKey: ['rounds'],
+    queryFn: () => RoundsService.getRounds(),
+    staleTime: 1000 * 60 * 60 * 24,
+  });
 
-  if (!isLoading && !roundId) return <p className='text-center text-sm'>Select a round to view it's categories.</p>;
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<Category | null>(null);
+
+  const addCategory = useMutation({
+    mutationFn: ({ name, icon, roundId }: { name: string; icon: string; roundId: number }) =>
+      CategoriesService.createOne({ name, icon, roundId }),
+  });
+
+  useEffect(() => {
+    if (categories.length === 0) {
+      return;
+    }
+    const defaultCategory = categories[0];
+    setSelectedCategory(defaultCategory);
+  }, [categories]);
+
+  useEffect(() => {
+    if (selectedCategory) {
+      setCategoryIdQueryParam(selectedCategory.id);
+    }
+  }, [selectedCategory]);
+
+  useEffect(() => {
+    if (rounds.length === 0) {
+      return;
+    }
+    const foundRound = rounds.find((round) => round.id === roundId) || null;
+    setCategories(foundRound?.categories ?? []);
+  }, [rounds, roundId]);
+
+  const deleteCategory = useMutation({
+    mutationFn: (categoryId: number) => CategoriesService.deleteOne(categoryId),
+  });
+
+  const editCategory = useMutation({
+    mutationFn: async ({ name, icon }: { name: string; icon: string }) => {
+      if (!selectedCategory?.id) throw new Error('No category selected');
+      return CategoriesService.editOne(selectedCategory.id, name, icon);
+    },
+  });
+
+  const setCategoryIdQueryParam = (categoryId: number) => {
+    router.navigate({ search: { ...search, categoryId }, reloadDocument: false, to: '/' });
+  };
+
+  if (!roundsLoading && !roundId) return <p className='text-center text-sm'>Select a round to view it's categories.</p>;
 
   return (
     <SidebarSectionList
       id='categories'
       isAdmin={isAdmin}
       title='Categories'
-      isLoading={isLoading}
+      isLoading={roundsLoading}
       items={
         categories?.map((category) => ({
           id: category.id,
           item: (
             <CategoryListButton
               category={category}
-              isSelected={sidebarState.selectedCategoryId === category.id}
-              onClick={() => sidebarState.setSelectedCategoryId(category.id)}
+              isSelected={selectedCategory?.id === category.id}
+              onClick={() => setSelectedCategory(category)}
               isAdmin={isAdmin}
-              onDelete={sidebarState.deleteCategory}
-              onEdit={sidebarState.editCategory}
+              onDelete={deleteCategory.mutate}
+              onEdit={(name, icon) => editCategory.mutate({ name, icon })}
             />
           ),
         })) ?? []
       }
-      addItem={<AddCategoryModal roundId={roundId} onSave={(name, icon, roundId) => addCategory(name, icon, roundId)} />}
+      addItem={<AddCategoryModal roundId={roundId} onSave={(name, icon, roundId) => addCategory.mutate({ name, icon, roundId })} />}
       maxItems={5}
     />
   );
--- packages/web/src/features/sidebar/index.tsx
+++ packages/web/src/features/sidebar/index.tsx
@@ -4,19 +4,37 @@ import LogosSection from '@/features/sidebar/components/logos-section';
 import { RoundList } from '@/features/sidebar/components/round-list/round-list';
 import { SettingsSection } from '@/features/sidebar/components/settings-section/settings-section';
 import { WakeUpLogo } from '@/features/sidebar/components/wakeup-logo';
-import { useSidebarStore } from '@/state/sidebar/sidebar-store';
+
+/* import { RoundsService } from '@/services/rounds-service';
+import { CompleteRound } from '@/types/rounds';
+import { useQuery } from '@tanstack/react-query';
+import { useSearch } from '@tanstack/react-router';
+import { useEffect, useState } from 'react'; */
 
 export function SidebarContent() {
-  const selectedRound = useSidebarStore((state) => state.selectedRound);
+  /*   const { roundId } = useSearch({ from: '/' });
+  const { data: rounds = [] } = useQuery({
+    queryKey: ['rounds'],
+    queryFn: () => RoundsService.getRounds(),
+    staleTime: 1000 * 60 * 60 * 24,
+  });
+  const [selectedRound, setSelectedRound] = useState<CompleteRound | null>(null);
+  useEffect(() => {
+    if (rounds.length === 0) {
+      return;
+    }
+    const foundRound = rounds.find((round) => round.id === roundId) || null;
+    setSelectedRound(foundRound);
+  }, [rounds, roundId]); */
 
   return (
-    <div className='flex flex-col items-start h-full gap-6 pb-16'>
+    <div className='flex h-full flex-col items-start gap-6 pb-16'>
       <img src={OmiLogo} alt='Optimism Making Impact Logo' className='w-[127px]' />
       <div className='flex flex-col justify-start gap-4 md:justify-between'>
         <div className='flex flex-col gap-4'>
           <RoundList />
           <hr />
-          {<CategoryList roundId={selectedRound?.id} categories={selectedRound?.categories ?? []} />}
+          {<CategoryList />}
           <hr />
           <LogosSection />
           <hr />
