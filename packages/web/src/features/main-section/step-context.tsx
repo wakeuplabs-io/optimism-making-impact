14069fde8c59b64ef85d20b5d76053500bb03807
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-03-19 09:44:32 -0300

    Add todo comment

--- packages/web/src/features/main-section/step-context.tsx
+++ packages/web/src/features/main-section/step-context.tsx
@@ -7,6 +7,7 @@ interface StepContextType {
 
 const StepContext = createContext<StepContextType | undefined>(undefined);
 
+// TODO: split this file contents: Fast refresh only works when a file only exports components. Use a new file to share constants or functions between components.eslint(react-refresh/only-export-components)
 export function useStepContext() {
   const context = useContext(StepContext);
   if (!context) {
