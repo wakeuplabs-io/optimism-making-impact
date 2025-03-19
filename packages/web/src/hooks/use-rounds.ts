ec8cbf08e84ae4fc9206c4379fd7c52327adcfed
Luciano Pérez Cerra <perezcerraluciano@gmail.com>
2025-03-19 16:45:10 -0300

    test useList as provider

--- packages/web/src/hooks/use-rounds.ts
+++ packages/web/src/hooks/use-rounds.ts
@@ -1,11 +1,11 @@
-import { RoundsContext } from "@/contexts/rounds/rounds-context";
-import { useContext } from "react";
+import { RoundsContext } from '@/contexts/rounds/rounds-context';
+import { useContext } from 'react';
 
 export function useRounds() {
   const context = useContext(RoundsContext);
 
   if (!context) {
-    throw new Error("useRounds must be used within a RoundsProvider");
+    throw new Error('useRounds must be used within a RoundsProvider');
   }
   return context;
 }
