ea63124517a58aa6c052342996a09f50dececb41
Matias Barro <matias.barro@gmail.com>
2025-03-19 15:16:06 -0300

    Add variants to icon-with-default

--- packages/web/src/components/icon-with-default.tsx
+++ packages/web/src/components/icon-with-default.tsx
@@ -1,15 +1,31 @@
 import { useIcons } from '@/hooks/use-icons';
+import { cn } from '@/lib/utils';
+import { cva } from 'class-variance-authority';
 
 interface IconWithDefaultProps {
   src: string;
   className?: string;
   defaultIcon?: string;
-  size?: 'md' | 'lg';
+  size?: 'sm' | 'base' | 'md' | 'lg';
 }
 
-export function IconWithDefault({ src, defaultIcon = 'blocks', className }: IconWithDefaultProps) {
+const iconVariant = cva('', {
+  variants: {
+    size: {
+      sm: 'h-4 w-4',
+      base: 'h-5 w-5',
+      md: 'h-6 w-6',
+      lg: 'h-8 w-8',
+    },
+  },
+  defaultVariants: {
+    size: 'base',
+  },
+});
+
+export function IconWithDefault({ src, size, defaultIcon = 'blocks', className }: IconWithDefaultProps) {
   const modalIcons = useIcons();
 
   const LucideIcon = modalIcons[src] || modalIcons[defaultIcon];
-  return <LucideIcon className={className} />;
+  return <LucideIcon className={cn(iconVariant({ size }), className)} />;
 }
