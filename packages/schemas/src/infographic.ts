c564d58ce052e0b088fa9930f87189710f327f35
Martin <59508264+martinc1991@users.noreply.github.com>
2025-03-20 10:36:10 -0300

    Fix: validate infogrpahic image to be url on submit (#143)

--- packages/schemas/src/infographic.ts
+++ packages/schemas/src/infographic.ts
@@ -1,9 +1,13 @@
 import { idValidator } from './common';
 import { z } from 'zod';
 
+const infogrpahicImageSchema = z.string().min(1, { message: 'Image is required' }).url({
+  message: 'Image must be a valid URL',
+});
+
 export const createInfographicBodySchema = z.object({
   markdown: z.string().min(1, { message: 'Markdown is required' }),
-  image: z.string().min(1, { message: 'Image is required' }),
+  image: infogrpahicImageSchema,
   stepId: z
     .number({
       required_error: 'Step is required',
@@ -15,7 +19,7 @@ export type CreateInfographicBody = z.infer<typeof createInfographicBodySchema>;
 
 export const updateInfographicBodySchema = z.object({
   markdown: z.string().min(1, { message: 'Markdown is required' }),
-  image: z.string().min(1, { message: 'Image is required' }),
+  image: infogrpahicImageSchema,
 });
 
 export type UpdateInfographicBody = z.infer<typeof updateInfographicBodySchema>;
