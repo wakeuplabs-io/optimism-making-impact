eef473971b4ac90303380f28d89af2f3f49cdbc9
Matias Barro <matias.barro@gmail.com>
2025-03-20 18:45:59 -0300

    Improve create round modal and trigger

--- packages/web/src/features/sidebar/components/create-round-modal.tsx
+++ packages/web/src/features/sidebar/components/create-round-modal.tsx
@@ -1,18 +1,25 @@
 import { Modal } from '@/components/modal';
 import { SidebarModalAddTrigger } from '@/features/sidebar/components/sidebar-modal-add-trigger';
+import { useToggle } from 'usehooks-ts';
 
 interface CreateRoundModalProps {
   onSave: () => void;
 }
 
 export function CreateRoundModal(props: CreateRoundModalProps) {
+  const [isOpen, toggleOpen] = useToggle(false);
+
   return (
-    <Modal
-      title='New round'
-      trigger={<SidebarModalAddTrigger label='New Round' />}
-      buttons={[{ label: 'Create', variant: 'primary', onClick: () => props.onSave() }]}
-    >
-      <span>You are about to create a new round. Are you sure?</span>
-    </Modal>
+    <>
+      <SidebarModalAddTrigger label='New Round' onClick={toggleOpen} />
+      <Modal
+        title='New round'
+        open={isOpen}
+        onOpenChange={toggleOpen}
+        buttons={[{ label: 'Create', variant: 'primary', onClick: () => props.onSave() }]}
+      >
+        <span>You are about to create a new round. Are you sure?</span>
+      </Modal>
+    </>
   );
 }
