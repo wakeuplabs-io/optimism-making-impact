bfe609ea972e7c06b45c9cc1a4cc63a34770ce2c
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-03-20 08:38:16 -0300

    Validate infogrpahic image to be url on submit

--- packages/web/src/lib/utils.ts
+++ packages/web/src/lib/utils.ts
@@ -15,20 +15,6 @@ export function isValidUrl(url: unknown) {
   return validUrlSchema.safeParse(url).success;
 }
 
-/**
- * Determines if a URL points to a valid image.
- * @param url - The URL to check.
- * @returns A promise that resolves to `true` if the URL is a valid image, otherwise `false`.
- */
-export async function isValidImageUrl(url: string): Promise<boolean> {
-  return new Promise((resolve) => {
-    const img = new Image();
-    img.onload = () => resolve(true); // If the image loads successfully, it is a valid image
-    img.onerror = () => resolve(false); // If an error occurs while loading, it's not a valid image
-    img.src = url; // Trigger the loading of the image
-  });
-}
-
 export function capitalizeFirst(string: string) {
   const lower = string.toLowerCase();
   return lower.charAt(0).toUpperCase() + lower.slice(1);
