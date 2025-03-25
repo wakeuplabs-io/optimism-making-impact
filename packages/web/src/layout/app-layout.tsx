00c1e9987ac13e959e83631a705aae1e6c1694a7
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-03-25 10:25:54 -0300

    Dont render steps and main section if no category is selected

--- packages/web/src/layout/app-layout.tsx
+++ packages/web/src/layout/app-layout.tsx
@@ -2,6 +2,7 @@ import { Toaster } from '@/components/ui/toaster';
 import { IS_DEVELOPMENT } from '@/config';
 import { RoundsProvider } from '@/contexts/rounds/rounds-provider';
 import { StepsProvider } from '@/contexts/steps/steps-provider';
+import { useQueryParams } from '@/hooks/use-query-params';
 import { useIsMobile } from '@/hooks/use-tresholds';
 import { IsAdminIndicator } from '@/layout/sections/is-admin-indicator';
 import { MainSection } from '@/layout/sections/main';
@@ -11,6 +12,7 @@ import { cn } from '@/lib/utils';
 
 export function AppLayout() {
   const isMobile = useIsMobile();
+  const { selectedCategoryId } = useQueryParams();
 
   return (
     <RoundsProvider>
@@ -18,13 +20,17 @@ export function AppLayout() {
         <div className='flex h-screen w-screen flex-col overflow-hidden lg:flex-row'>
           <SidebarSection />
           <div
-            className={cn('flex flex-1 flex-col-reverse bg-[#F1F4F9] lg:flex-col lg:pb-8 lg:px-16', {
+            className={cn('flex flex-1 flex-col-reverse bg-[#F1F4F9] lg:flex-col lg:px-16 lg:pb-8', {
               'overflow-y-auto': !isMobile,
               'overflow-hidden': isMobile,
             })}
           >
-            <StepsSection />
-            <MainSection />
+            {selectedCategoryId && (
+              <>
+                <StepsSection />
+                <MainSection />
+              </>
+            )}
           </div>
           <Toaster />
           {IS_DEVELOPMENT && <IsAdminIndicator />}
