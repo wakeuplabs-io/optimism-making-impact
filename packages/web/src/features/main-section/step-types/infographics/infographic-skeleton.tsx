993880344489ef2240db5c0374e9fdd47a6646bb
Facundo De Lorenzo <fdelorenzo.dev@gmail.com>
2025-03-26 11:23:00 -0300

    update infographic skeleton & typescript-transform-paths package

--- packages/web/src/features/main-section/step-types/infographics/infographic-skeleton.tsx
+++ packages/web/src/features/main-section/step-types/infographics/infographic-skeleton.tsx
@@ -9,15 +9,21 @@ export function InfographicStepSkeleton() {
           <div
             key={x}
             className={cn('flex w-full flex-col-reverse items-center justify-center gap-6 rounded-[10px] xl:flex-row xl:gap-12', {
-              'xl:order-2': x % 2 === 0,
-              'xl:order-1': !(x % 2 === 0),
+              'xl:flex-row-reverse': x % 2 === 0,
+              'xl:flex-row': !(x % 2 === 0),
             })}
           >
             <div className='flex w-full flex-grow flex-col xl:w-1/2'>
-              <Skeleton count={1} height={64} containerClassName='w-2/3' borderRadius={10} className='my-10 w-max px-3' />
+              <Skeleton
+                count={1}
+                height={64}
+                containerClassName='w-2/3'
+                borderRadius={10}
+                className='my-10 w-max px-3'
+              />
               <Skeleton count={2} height={36} containerClassName='w-full' borderRadius={10} className='my-2 w-max px-3' />
             </div>
-            <Skeleton containerClassName='items-center justify-center w-full xl:w-1/2' className='aspect-video h-[274px] xl:h-[320px]' />
+            <Skeleton containerClassName='items-center justify-center w-full md:w-1/2' className='aspect-video h-[274px] xl:h-[320px]' />
           </div>
         ))}
       </div>
