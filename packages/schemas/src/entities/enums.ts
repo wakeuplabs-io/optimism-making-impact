653162095ea4a2d33c6df274c53569cb22666ad1
Luciano Pérez Cerra <perezcerraluciano@gmail.com>
2025-03-27 16:33:06 -0300

    fix prettier pluging setup

--- packages/schemas/src/entities/enums.ts
+++ packages/schemas/src/entities/enums.ts
@@ -1,10 +1,9 @@
-import { z } from 'zod';
 import { StepType as PrismaStepType, CardStrength as PrismaCardStrength, Color as PrismaColor } from '@optimism-making-impact/prisma';
+import { z } from 'zod';
 
 export const stepTypes = Object.values(PrismaStepType);
 export const stepTypeSchema = z.nativeEnum(PrismaStepType);
 export type StepType = z.infer<typeof stepTypeSchema>;
-
 
 export const cardStrengthSchema = z.nativeEnum(PrismaCardStrength);
 export type CardStrength = z.infer<typeof cardStrengthSchema>;
