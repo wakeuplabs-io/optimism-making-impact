67702a20e98d3eeaed7788666cd1338bbaddfadc
Luciano Perez Cerra <perezcerraluciano@gmail.com>
2025-03-28 09:47:20 -0300

    Chore/omi 149/prisma types share (#165)

--- packages/schemas/src/entities/items.ts
+++ packages/schemas/src/entities/items.ts
@@ -1,4 +1,4 @@
-import { commonFieldsSchema } from './helpers';
+import { commonFieldsSchema } from './helpers.js';
 import { z } from 'zod';
 
 export const itemSchema = commonFieldsSchema.extend({
