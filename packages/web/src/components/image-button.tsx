f562e0ed07d5238468f383909ce5981b7a232cf9
Luciano Pérez Cerra <perezcerraluciano@gmail.com>
2025-04-08 18:06:38 -0300

    adjust paragraph margins

--- packages/web/src/components/image-button.tsx
+++ packages/web/src/components/image-button.tsx
@@ -9,10 +9,7 @@ export interface ImageButtonProps extends React.ButtonHTMLAttributes<HTMLButtonE
 
 export function ImageButton({ src, className, alt, ...props }: ImageButtonProps) {
   return (
-    <button
-      className='relative disabled:cursor-not-allowed disabled:opacity-50 disabled:brightness-75 disabled:grayscale'
-      {...props}
-    >
+    <button className='relative disabled:cursor-not-allowed disabled:opacity-50 disabled:brightness-75 disabled:grayscale' {...props}>
       <img src={src} alt={alt} className={cn('transition-all duration-300 hover:brightness-75 hover:rounded-lg inline-block', className)} />
     </button>
   );
--- packages/web/src/features/sidebar/components/create-round-modal.tsx
+++ packages/web/src/features/sidebar/components/create-round-modal.tsx
@@ -7,7 +7,6 @@ interface CreateRoundModalProps {
   isDisabled?: boolean;
   onSave: () => void;
 }
-
 export function CreateRoundModal({ isDisabled, onSave }: CreateRoundModalProps) {
   const [isOpen, toggleOpen] = useToggle(false);
 
@@ -24,14 +23,14 @@ export function CreateRoundModal({ isDisabled, onSave }: CreateRoundModalProps) 
         ]}
       >
         <div className='flex flex-col space-y-4 mb-6'>
-          <p className='font-bold text-base'>You're about to create a new round.</p>
+          <p className='font-bold text-base  ml-[32px]'>You're about to create a new round.</p>
 
           <div className='flex items-start space-x-3 my-1'>
             <AlertCircle className='text-gray-500 mt-0.5 flex-shrink-0' size={20} />
-            <p className='text-gray-600 text-sm'>This will duplicate the latest round along with all its associated links.</p>
+            <p className='text-gray-600 text-sm '>This will duplicate the latest round along with all its associated links.</p>
           </div>
 
-          <p className='font-bold text-base'>Are you sure you want to proceed?</p>
+          <p className='font-bold text-base ml-[32px]'>Are you sure you want to proceed?</p>
         </div>
       </Modal>
     </>
