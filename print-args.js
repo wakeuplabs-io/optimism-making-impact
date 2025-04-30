453ee2d35b098eacce02f50b300cb57b5207745f
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-04-30 20:32:37 -0300

    s

--- print-args.js
+++ print-args.js
@@ -1,3 +1,12 @@
 // print-args.js
 const args = process.argv.slice(2);
 console.log('Received arguments:', args);
+
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+for (let i = 0; i < alphabet.length; i++) {
+  const letter = alphabet[i];
+  if (args[0].includes(letter)) {
+    console.log('letter is included', i, letter);
+  }
+}
