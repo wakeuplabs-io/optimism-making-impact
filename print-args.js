e1b915fe25c2f2137e1d1bae8476e32f471fa58c
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-04-30 20:36:20 -0300

    a

--- print-args.js
+++ print-args.js
@@ -5,6 +5,7 @@ console.log('Received arguments:', args);
 const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
 for (let i = 0; i < alphabet.length; i++) {
+  console.log('INSIDE FOR', i);
   const letter = alphabet[i];
   if (args[0].includes(letter)) {
     console.log('letter is included', i, letter);
